98c1cdc5da6210560f22b94c6785cb1bff5db457
Monish Singhal <42601474+mynameismon@users.noreply.github.com>
2023-05-28 23:47:10 +0530

    finish inductive junction

--- inductive_4_way.ino
+++ inductive_4_way.ino
@@ -3,7 +3,7 @@
 
 #include <Servo.h>
 
-int type; // Type of circuit 
+int type = 1; // Type of circuit 
 // 1 - 4 junction with 4 ultrasonic sensors
 // 0 - 3 junction with 3 ultrasonic sensors
 
@@ -16,44 +16,17 @@ const int inductivePin_4 = 14;  // Pin connected to the inductive sensor
 // Constants (won't change)
 const int led_red_1 = 22;
 const int led_red_2 = 23;
-const int led_red_3 = 35;
+const int led_red_3 = 25;
 const int led_red_4 = 15;
 
 const int led_green_1 = 21;
 const int led_green_2 = 27;
-const int led_green_3 = 34;
+const int led_green_3 = 33;
 const int led_green_4 = 19;
 
 const int servoPin = 13;  // Pin connected to the servo motor
 
 Servo servo;  // Create a servo object
-
-// const unsigned int maxDistance = 200;  // Maximum distance to measure (in centimeters)
-
-// Object instances for ultrasonic sensors
-// NewPing sonar1(trigPin1, echoPin1, maxDistance);
-// NewPing sonar2(trigPin2, echoPin2, maxDistance);
-// NewPing sonar3(trigPin3, echoPin3, maxDistance);
-// NewPing sonar4(trigPin4, echoPin4, maxDistance);
-
-// Variables (will change)
-// unsigned long currentMillis = 0;   // Current time in milliseconds
-// unsigned long previousMillis = 0;  // Previous time for reading sensors
-
-// int is_1_red= 0;
-// int is_2_red= 0;
-// int is_3_red= 0;
-// int is_4_red= 0;
-
-// int red_count_1= 0;
-// int red_count_2= 0;
-// int red_count_3= 0;
-// int red_count_4= 0;
-
-// int green_count_1= 0;
-// int green_count_2= 0;
-// int green_count_3= 0;
-// int green_count_4= 0;
 
 typedef enum tl_status {
   RED = 0,
@@ -68,9 +41,8 @@ int light_index;
 int multiplier;
 int delay_per_read;
 int threshold_time;
-int flag_max_hit; // 
+int flag_max_hit;
 int servoPosition = 0;  // Initial position of the servo motor
-
 
 void setup() {
   Serial.begin(9600);
@@ -99,14 +71,16 @@ void setup() {
   delay_per_read = 250;
   threshold_time = 30000;
   flag_max_hit = 0;
-  type = 0;
+  type = 1;
 
   servo.attach(servoPin);  // Attach the servo to the specified pin
   servo.write(servoPosition);  // Set the initial position of the servo motor
-
 }
 
 void loop() {
+
+  Serial.printf("LED red  : %d %d %d %d\n", digitalRead(led_red_1), digitalRead(led_red_2), digitalRead(led_red_3), digitalRead(led_red_4));
+  Serial.printf("LED green: %d %d %d %d\n", digitalRead(led_green_1), digitalRead(led_green_2), digitalRead(led_green_3), digitalRead(led_green_4));
 
   int max_cars=threshold_time/multiplier;
   
@@ -126,40 +100,40 @@ void loop() {
     break;
   }
   case 1: {
-    digitalWrite(led_red_2,LOW);
     digitalWrite(led_red_1,HIGH);
+    digitalWrite(led_red_2,LOW);
     digitalWrite(led_red_3,HIGH);
     digitalWrite(led_red_4,HIGH);
     
-    digitalWrite(led_green_2,HIGH);
     digitalWrite(led_green_1,LOW);
+    digitalWrite(led_green_2,HIGH);
     digitalWrite(led_green_3,LOW);
     digitalWrite(led_green_4,LOW);
     break;
   }
   case 2: {
-    digitalWrite(led_red_3,LOW);
-    digitalWrite(led_red_2,HIGH);
     digitalWrite(led_red_1,HIGH);
+    digitalWrite(led_red_2,HIGH);
+    digitalWrite(led_red_3,LOW);
     digitalWrite(led_red_4,HIGH);
     
-    digitalWrite(led_green_3,HIGH);
-    digitalWrite(led_green_2,LOW);
     digitalWrite(led_green_1,LOW);
+    digitalWrite(led_green_2,LOW);
+    digitalWrite(led_green_3,HIGH);
     digitalWrite(led_green_4,LOW);
     break;
   }
   case 3: {
     if (type == 3) break;
-    digitalWrite(led_red_4,LOW);
+    digitalWrite(led_red_1,HIGH);
     digitalWrite(led_red_2,HIGH);
     digitalWrite(led_red_3,HIGH);
-    digitalWrite(led_red_1,HIGH);
+    digitalWrite(led_red_4,LOW);
     
-    digitalWrite(led_green_4,HIGH);
+    digitalWrite(led_green_1,LOW);
     digitalWrite(led_green_2,LOW);
     digitalWrite(led_green_3,LOW);
-    digitalWrite(led_green_1,LOW);
+    digitalWrite(led_green_4,HIGH);
     break;
   }
   default: {
@@ -188,7 +162,7 @@ void loop() {
     count_arr[light_index] = 0;
   }
   // If junction still has cars
-  else if(flag_max_hit<max_cars && count_arr[light_index] > max_cars ){
+  else if(flag_max_hit < max_cars && count_arr[light_index] > max_cars ){
     Timer[light_index] += multiplier*max_cars;
     flag_max_hit+=count_arr[light_index];
     count_arr[light_index] -= max_cars;
@@ -196,11 +170,11 @@ void loop() {
 
   Timer[light_index] -= 1;
 
-  count_arr[0] += read(inductivePin_1);  
-  count_arr[1] += read(inductivePin_2);
-  count_arr[2] += read(inductivePin_3);
+  count_arr[0] += read_from_pin(inductivePin_1);  
+  count_arr[1] += read_from_pin(inductivePin_2);
+  count_arr[2] += read_from_pin(inductivePin_3);
   if(type == 1)
-    count_arr[3] += read(inductivePin_4);
+    count_arr[3] += read_from_pin(inductivePin_4);
 
   // DEBUG PRINTS
   Serial.println("Count arr:-");
