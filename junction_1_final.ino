30fe7fd545792e83a21ab42f1d5fe89ff6e63719
Debangan-MishraIIIT <117065781+Debangan-MishraIIIT@users.noreply.github.com>
2023-05-29 22:44:50 +0530

    Update junction_1_final.ino

--- junction_1_final.ino
+++ junction_1_final.ino
@@ -1,4 +1,5 @@
 #include "Ultrasonic.h"
+#include "om2m.h"
 #include <Servo.h>
 
 int type = 1; // Type of circuit 
@@ -69,6 +70,12 @@ int servoPosition4 = 0;  // Initial position of the servo motor
 void setup() {
   Serial.begin(9600);
 
+  WiFi.begin(MAIN_SSID, MAIN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("#");
+  }
+
   // Initialise pinmodes for all LEDS
   pinMode(led_red_1,OUTPUT);
   pinMode(led_red_2,OUTPUT);
@@ -106,6 +113,11 @@ void setup() {
 }
 
 void loop() {
+  int a= digitalRead(led_green_1);
+  int b= digitalRead(led_green_2);
+  int c= digitalRead(led_green_3);
+  int d= digitalRead(led_green_4);
+  int arr2[]= {a, b, c, d};
 
   Serial.printf("LED red  : %d %d %d %d\n", digitalRead(led_red_1), digitalRead(led_red_2), digitalRead(led_red_3), digitalRead(led_red_4));
   Serial.printf("LED green: %d %d %d %d\n", digitalRead(led_green_1), digitalRead(led_green_2), digitalRead(led_green_3), digitalRead(led_green_4));
@@ -227,6 +239,7 @@ void loop() {
   if(type == 1)  Serial.println(Timer[3]);
   Serial.println();
   // END DEBUG PRINTS
+  write_to_om2m(type, count_arr, arr2, "Junction-1/Data");
 
   if(Timer[light_index] <= 0) {
     // reset all signal variables
