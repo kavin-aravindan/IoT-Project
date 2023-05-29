3dae484b050ad1f6f8975321777279d79f5ed6c1
Debangan-MishraIIIT <117065781+Debangan-MishraIIIT@users.noreply.github.com>
2023-05-30 02:53:50 +0530

    Update junction_4_final.ino

--- junction_4_final.ino
+++ junction_4_final.ino
@@ -6,6 +6,19 @@
 
 int type = 0; // Type of circuit 
 
+#define DEBUG
+
+#ifdef DEBUG
+const uint8_t max_delay = 0;
+uint8_t dont_print_delay = 0;
+const uint32_t delay_per_read = 250;
+#else
+const uint8_t max_delay = 63;
+uint8_t dont_print_delay = 63;
+const uint32_t delay_per_read = 1;
+#endif
+
+
 const int trigPin1 = 33;  // Trigger pin for sensor 1
 const int echoPin1 = 32;  // Echo pin for sensor 1
 
@@ -35,8 +48,6 @@ typedef enum tl_status {
 } tl_status_t;
 
 
-
-
 Ultrasonic ultrasonic1(trigPin1,echoPin1);
 Ultrasonic ultrasonic2(trigPin2,echoPin2);
 Ultrasonic ultrasonic3(trigPin3,echoPin3);
@@ -47,7 +58,6 @@ tl_status_t signal[4];  // 1 for green and 0 for red
 int Timer[4];   // 0 for red and time remaining in millisecs for the green light
 int light_index;
 int multiplier;
-int delay_per_read;
 int threshold_time;
 int flag_max_hit;
 
@@ -71,6 +81,7 @@ void setup() {
   pinMode(led_green_3,OUTPUT);
   pinMode(led_green_4,OUTPUT);
 
+  Serial.printf("Intial print delay: %d\n", dont_print_delay);
   
   light_index = 0;
   
@@ -81,7 +92,6 @@ void setup() {
   Timer[light_index] = 5000;
   signal[light_index] = GREEN;
   multiplier = 1250;
-  delay_per_read = 250;
   threshold_time = 30000;
   flag_max_hit = 0;
   type = 0;
@@ -95,10 +105,6 @@ void loop() {
   int c= digitalRead(led_green_3);
   int d= digitalRead(led_green_4);
   int arr2[]= {a, b, c, d};
-
-  Serial.printf("LED red  : %d %d %d %d\n", digitalRead(led_red_1), digitalRead(led_red_2), digitalRead(led_red_3), digitalRead(led_red_4));
-  Serial.printf("LED green: %d %d %d %d\n", digitalRead(led_green_1), digitalRead(led_green_2), digitalRead(led_green_3), digitalRead(led_green_4));
-
   int max_cars=threshold_time/multiplier;
   
   signal[light_index] = GREEN;
@@ -159,8 +165,6 @@ void loop() {
   }
 
 
-     
-
   /////// LED green trigger
   // If the junction is empty 
   if (flag_max_hit < max_cars &&
@@ -182,25 +186,36 @@ void loop() {
   count_arr[0] += read_from_pin(ultrasonic1);  
   count_arr[1] += read_from_pin(ultrasonic2);
   count_arr[2] += read_from_pin(ultrasonic3);
-  if(type == 1)
-    // count_arr[3] += read_from_pin(ultrasonic4);
+  if(type == 1) {
+    //count_arr[3] += read_from_pin(ultrasonic4);
+  }
 
-  // DEBUG PRINTS
-  Serial.println("Count arr:-");
-  Serial.println(count_arr[0]);
-  Serial.println(count_arr[1]);
-  Serial.println(count_arr[2]);
-  if(type == 1) Serial.println(count_arr[3]);
-  Serial.println();
+  if (!dont_print_delay) {
+    Serial.printf("LED red  : %d %d %d %d\n", digitalRead(led_red_1), digitalRead(led_red_2), digitalRead(led_red_3), digitalRead(led_red_4));
+    Serial.printf("LED green: %d %d %d %d\n", digitalRead(led_green_1), digitalRead(led_green_2), digitalRead(led_green_3), digitalRead(led_green_4));
+    // DEBUG PRINTS
+    Serial.println("Count arr:-");
+    Serial.println(count_arr[0]);
+    Serial.println(count_arr[1]);
+    Serial.println(count_arr[2]);
+    if(type == 1) Serial.println(count_arr[3]);
+    Serial.println();
 
-  Serial.println("Timer:-");
-  Serial.println(Timer[0]);
-  Serial.println(Timer[1]);
-  Serial.println(Timer[2]);
-  if(type == 1)  Serial.println(Timer[3]);
-  Serial.println();
-  // END DEBUG PRINTS
-  write_to_om2m(type, count_arr, arr2, "Junction-4/Data");
+    Serial.println("Timer:-");
+    Serial.println(Timer[0]);
+    Serial.println(Timer[1]);
+    Serial.println(Timer[2]);
+    if(type == 1)  Serial.println(Timer[3]);
+    Serial.println();
+    // END DEBUG PRINTS
+    count_arr[light_index] += flag_max_hit;
+    write_to_om2m(type, count_arr, arr2, "Junction-4/Data");
+    count_arr[light_index] -= flag_max_hit;
+    dont_print_delay = max_delay;
+  } else {
+    dont_print_delay--;
+    Serial.println(dont_print_delay);
+  }
 
   if(Timer[light_index] <= 0) {
     // reset all signal variables
